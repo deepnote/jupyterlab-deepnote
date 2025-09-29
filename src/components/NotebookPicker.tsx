50e853693f0835406df3a8d6a4b53e5391802e15
Andy Jakubowski <hello@andyjakubowski.com>
2025-09-29 15:21:29 +0200

    Extract `NotebookPicker` to own file
    Signed-off-by: Andy Jakubowski <hello@andyjakubowski.com>

--- src/components/NotebookPicker.tsx
+++ src/components/NotebookPicker.tsx
@@ -1,81 +1,9 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
-} from '@jupyterlab/application';
 import React from 'react';
-import { IToolbarWidgetRegistry, ReactWidget } from '@jupyterlab/apputils';
-import {
-  INotebookWidgetFactory,
-  NotebookPanel,
-  NotebookWidgetFactory
-} from '@jupyterlab/notebook';
-import { Widget } from '@lumino/widgets';
+import { ReactWidget } from '@jupyterlab/apputils';
+import { NotebookPanel } from '@jupyterlab/notebook';
 import { HTMLSelect } from '@jupyterlab/ui-components';
-import { ContentsManager } from '@jupyterlab/services';
-import {
-  DeepnoteContentProvider,
-  deepnoteContentProviderName
-} from './deepnote-content-provider';
 
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-deepnote:plugin',
-  description: 'Open .deepnote files as notebooks.',
-  autoStart: true,
-  requires: [INotebookWidgetFactory, IToolbarWidgetRegistry],
-  activate: (
-    app: JupyterFrontEnd,
-    notebookWidgetFactory: NotebookWidgetFactory,
-    toolbarRegistry: IToolbarWidgetRegistry
-  ) => {
-    const drive = (app.serviceManager.contents as ContentsManager).defaultDrive;
-    const registry = drive?.contentProviderRegistry;
-    if (!registry) {
-      // If content provider is a non-essential feature and support for JupyterLab <4.4 is desired:
-      console.error(
-        'Cannot initialize content provider: no content provider registry.'
-      );
-      return;
-    }
-    const deepnoteContentProvider = new DeepnoteContentProvider({
-      // These options are only required if extending the `RestContentProvider`.
-      apiEndpoint: '/api/contents',
-      serverSettings: app.serviceManager.serverSettings
-    });
-    registry.register(deepnoteContentProviderName, deepnoteContentProvider);
-    notebookWidgetFactory.contentProviderId = deepnoteContentProviderName;
-
-    app.docRegistry.addFileType(
-      {
-        name: 'deepnote',
-        displayName: 'Deepnote Notebook',
-        extensions: ['.deepnote'],
-        mimeTypes: ['text/yaml', 'application/x-yaml'],
-        fileFormat: 'text',
-        contentType: 'file'
-      },
-      [notebookWidgetFactory.name]
-    );
-
-    app.docRegistry.setDefaultWidgetFactory(
-      'deepnote',
-      notebookWidgetFactory.name
-    );
-
-    toolbarRegistry.addFactory<NotebookPanel>(
-      notebookWidgetFactory.name,
-      'deepnote:switch-notebook',
-      panel => {
-        if (!panel.context.path.endsWith('.deepnote')) {
-          return new Widget(); // don’t render for .ipynb or others
-        }
-
-        return new NotebookPicker(panel);
-      }
-    );
-  }
-};
-
-class NotebookPicker extends ReactWidget {
+export class NotebookPicker extends ReactWidget {
   private selected: string | null = null;
 
   constructor(private panel: NotebookPanel) {
@@ -195,5 +123,3 @@ class NotebookPicker extends ReactWidget {
     );
   }
 }
-
-export default plugin;
